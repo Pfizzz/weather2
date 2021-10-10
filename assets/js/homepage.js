5cb34d103cecd8d0ff67fc01db678aba36a7d81e
Pfizz <pfiste80@gmail.com>
2021-10-09 22:18:02 -0400

    functional api call

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,39 @@
-var test = function() {
-    console.log("hooked up");
+var key = "7f3f0a1f90357c304e6068c79749f336";
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#cityname");
+
+var getWeatherInfo = function(cityname) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + key;
+    console.log(apiUrl);
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            } else {
+                alert("something went terribly wrong");
+            }
+        })
 }
 
-test();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log("clicked");
+    var cityname = cityInputEl.value.trim();
+        if (cityname) {
+            getWeatherInfo(cityname);
+            cityInputEl.value = '';
+        } else {
+            alert("enter a city name bro");
+        }
+}
+
+var displayWeather = function() {
+
+}
+
+
+
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
