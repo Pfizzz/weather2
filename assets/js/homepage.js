eee2be38bffd6af85c49dd9bf5da45935d1c3070
Pfizz <pfiste80@gmail.com>
2021-10-18 08:24:03 -0400

    added five-day fetch and display function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,51 +1,68 @@
-var key = "7f3f0a1f90357c304e6068c79749f336";
-var cityFormEl = document.querySelector("#city-form");
-var cityInputEl = document.querySelector("#cityname");
-var weatherContainerEl = document.querySelector("#weathers-container");
-var weatherSearchTerm = document.querySelector("#weather-search-term");
+const key = "7f3f0a1f90357c304e6068c79749f336";
+const cityFormEl = document.querySelector("#city-form");
+const cityInputEl = document.querySelector("#cityname");
+const weatherContainerEl = document.querySelector("#weathers-container");
+const weatherSearchTerm = document.querySelector("#weather-search-term");
 
-var getWeatherInfo = function(cityname) {
+const getWeatherInfo = (cityname) => {
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + key + "&units=imperial";
     console.log(apiUrl);
     fetch(apiUrl)
-        .then(function(response) {
+        .then(function (response) {
             if (response.ok) {
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     // console.log(data);
                     displayWeather(data, cityname);
                 })
             } else {
-                alert("something went terribly wrong");
-            }
-        })
-}
-
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    // console.log("clicked");
-    var cityname = cityInputEl.value.trim();
-        if (cityname) {
-            getWeatherInfo(cityname);
-            cityInputEl.value = '';
-        } else {
-            alert("enter a city name bro");
+                alert("response error with fetching weather data");
         }
-}
+    });
+    getFiveWeatherInfo(cityname);
+};
 
-var displayWeather = function(data, searchTerm) {
+// function to get five day forecast
+const getFiveWeatherInfo = (cityname) => {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&appid=" + key + "&units=imperial";
+    console.log(apiUrl);
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    // console.log(data);
+                    displayFiveWeatherData(data, cityname);
+                })
+            } else {
+                alert("response error with get 5-day fetch request")
+            }
+        })         
+};
+
+const formSubmitHandler = (event) => {
+    event.preventDefault();
+    var cityname = cityInputEl.value.trim();
+    if (cityname) {
+        getWeatherInfo(cityname);
+        cityInputEl.value = '';
+    } else {
+        alert("Please enter a city name!");
+    }
+};
+
+const displayWeather = (data, searchTerm) => {
     // console.log(data);
     // console.log(searchTerm);
     // console.log("displayweather works");
     weatherContainerEl.textContent = '';
     weatherSearchTerm.textContent = searchTerm;
     var currentWeather = document.createElement("div");
-    var titleEl= document.createElement("span");
-    titleEl.innerHTML = "<h3>"+data.name + data.dt +"</h3>" +
-                        "Current Temperature: " + data.main.temp + "&#8457" +
-                        "</br> Windspeed: " + data.wind.speed + " mph" +
-                        "</br> Humidity: " + data.main.humidity +"%"
-                        // "</br> UV index: " +data.uvi;
-    ;
+    var titleEl = document.createElement("span");
+    titleEl.innerHTML = "<h3>" + data.name + data.dt + "</h3>" +
+        "Current Temperature: " + data.main.temp + "&#8457" +
+        "</br> Windspeed: " + data.wind.speed + " mph" +
+        "</br> Humidity: " + data.main.humidity + "%"
+        // "</br> UV index: " +data.uvi;
+        ;
 
     // console.log(data.main);
 
@@ -53,8 +70,11 @@ var displayWeather = function(data, searchTerm) {
     // currentWeather.appendChild(windEl);
 
     weatherContainerEl.appendChild(currentWeather);
+};
 
-
+const displayFiveWeatherData = (data, cityname) => {
+    console.log("connect to display five day");
+    console.log(data);
 }
 
 
